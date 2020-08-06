2cf3d43ec4f083eb8ca006db04eb70e8dfb93674
Grigory-Rylov <grishberg@gmail.com>
2020-08-06 23:25:41 +0300

    v0.3.0

--- README.md
+++ README.md
@@ -4,6 +4,22 @@
 # Simple Binary Preferences.
 Reads and writes values into binary format.
 
+## Performance
+Android shared preferences VS simple binary preferences for 10 launches:
+android : 3.213542 ms VS binary prefs: 0.755834 ms
+android : 3.835781 ms VS binary prefs: 2.886511 ms
+android : 4.19849 ms VS binary prefs: 2.704844 ms
+android : 6.501615 ms VS binary prefs: 2.619844 ms
+android : 3.869219 ms VS binary prefs: 3.087813 ms
+android : 3.427708 ms VS binary prefs: 2.950833 ms
+android : 4.537761 ms VS binary prefs: 3.129376 ms
+android : 6.586198 ms VS binary prefs: 2.668959 ms
+android : 7.719167 ms VS binary prefs: 2.840365 ms
+android : 4.903334 ms VS binary prefs: 3.027344 ms
+android : 4.220209 ms VS binary prefs: 3.094531 ms
+
+measured on Nexus 5X
+
 ## Dependencies
 
 1) Add it in your root build.gradle at the end of repositories:
--- demo/src/main/java/com/github/grishberg/simplebinarypreferences/MainActivity.java
+++ demo/src/main/java/com/github/grishberg/simplebinarypreferences/MainActivity.java
@@ -1,35 +1,98 @@
 package com.github.grishberg.simplebinarypreferences;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.github.grishberg.binarypreferences.BinaryPreferences;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    private static final String ALREADY_LAUNCHED = "launched";
-    private static final String LAUNCH_COUNT = "count";
+    private static final String PERF_TAG = "PROFILING";
+
+    private static final String ALREADY_LAUNCHED_KEY = "launched";
+    private static final String LAUNCH_COUNT_KEY = "count";
+    private static final String STRING_KEY = "string_key";
+    private static final String MULTIPLE_LONG_KEY_PREFIX = "multiple_long_";
+    private static final double NANOS = 1000000.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        boolean launched1 = false;
+        boolean launched2 = false;
+        int launchCount1 = 0;
+        int launchCount2 = 0;
+        String storedValue1;
+        String storedValue2;
 
+        long start;
+
+        Set<String> someSet = new HashSet<>();
+        someSet.add("String1");
+        someSet.add("String2");
+        someSet.add("String3");
+        someSet.add("String4");
+        someSet.add("String5");
+        someSet.add("String6");
+        someSet.add("String7");
+        someSet.add("String8");
+        someSet.add("String9");
+        someSet.add("String10");
+
+        start = System.nanoTime();
         SharedPreferences preferences = new BinaryPreferences(this, "settings");
-        boolean launched = preferences.getBoolean(ALREADY_LAUNCHED, false);
-        int launchCount = preferences.getInt(LAUNCH_COUNT, 0);
+        launched2 = preferences.getBoolean(ALREADY_LAUNCHED_KEY, false);
+        launchCount2 = preferences.getInt(LAUNCH_COUNT_KEY, 0);
+        storedValue2 = preferences.getString(STRING_KEY, "");
+        long duration = System.nanoTime() - start;
 
-        if (launched) {
-            Log.e(TAG, "already launched " + launchCount + " times");
+        start = System.nanoTime();
+        SharedPreferences xmlPreferences = getSharedPreferences("xml_settings", Context.MODE_PRIVATE);
+        launched1 = xmlPreferences.getBoolean(ALREADY_LAUNCHED_KEY, false);
+        launchCount1 = xmlPreferences.getInt(LAUNCH_COUNT_KEY, 0);
+        storedValue1 = xmlPreferences.getString(STRING_KEY, "");
+        long xmlPreferencesDuration = System.nanoTime() - start;
+
+        Log.e(TAG, "stored strings: " + storedValue1 + " - " + storedValue2);
+        Log.e(PERF_TAG, "android : " + xmlPreferencesDuration / NANOS + " ms VS binary prefs: " + duration / NANOS + " ms");
+        if (launched1 | launched2) {
+            Log.e(TAG, "already launched " + launchCount1 + ":" + launchCount2 + " times");
         } else {
             Log.e(TAG, "first time launched");
         }
         SharedPreferences.Editor editor = preferences.edit();
-        editor.putInt(LAUNCH_COUNT, launchCount + 1);
-        editor.putBoolean(ALREADY_LAUNCHED, true);
+        editor.putInt(LAUNCH_COUNT_KEY, launchCount1 + 1);
+        editor.putBoolean(ALREADY_LAUNCHED_KEY, true);
+        editor.putString(STRING_KEY, getPackageName());
+        editor.putStringSet("STRING_SET_KEY", someSet);
+
+        SharedPreferences.Editor editorXml = xmlPreferences.edit();
+        editorXml.putInt(LAUNCH_COUNT_KEY, launchCount2 + 1);
+        editorXml.putBoolean(ALREADY_LAUNCHED_KEY, true);
+        editorXml.putString(STRING_KEY, getPackageName());
+        editorXml.putStringSet("STRING_SET_KEY", someSet);
+
+        for (int i = 0; i < 20; i++) {
+            editor.putLong(MULTIPLE_LONG_KEY_PREFIX + i, i);
+        }
+
+        for (int i = 0; i < 20; i++) {
+            editor.putString("multiple_string_" + i, "some string value " + i);
+        }
+
+        for (int i = 0; i < 20; i++) {
+            editor.putFloat("multiple_float_" + i, i);
+        }
+
         editor.apply();
+        editorXml.apply();
     }
 }
